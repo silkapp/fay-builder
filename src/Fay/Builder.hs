6a3d26e6b2e0ec0d1ff29d8a25943b817ac39f2d
Adam Bergmark <adam@bergmark.nl>
2013-12-12 18:31:39 +0100

    fay-builder: Add default Cabal hooks

--- src/Fay/Builder.hs
+++ src/Fay/Builder.hs
@@ -1,31 +1,44 @@
-module Fay.Builder where
+module Fay.Builder
+ ( readPackageDescription
+ , build
+ , listField
+ , listField_
+ , field
+ , field_
+ , fayConfig
+ , defaultFayHook
+ , postBuildHook
+ ) where
 
 import Control.Monad
 import Data.Default
+import Data.List
 import Data.List.Split
 import Data.Maybe
-import Distribution.PackageDescription
-import Fay
-import Fay.Compiler.Config
 import System.Directory
 import System.FilePath
-import qualified Data.Text              as T
-import qualified Distribution.Verbosity as Verbosity
-import qualified Distribution.PackageDescription as PD
-import qualified Distribution.PackageDescription.Parse as PD
+import qualified Data.Text as T
+
+import Distribution.PackageDescription
+import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
+import Fay
+import Fay.Compiler.Config
+import qualified Distribution.PackageDescription.Parse as PD (readPackageDescription)
+import qualified Distribution.Verbosity                as Verbosity
+
 
 readPackageDescription :: FilePath -> IO PackageDescription
-readPackageDescription = PD.readPackageDescription Verbosity.silent >=> return . PD.packageDescription
+readPackageDescription = PD.readPackageDescription Verbosity.silent >=> return . packageDescription
 
-foo :: PackageDescription -> Maybe FilePath -> IO ()
-foo packageDesc pkgDb = do
-  let packages     = listField "x-fay-packages"      packageDesc
-      roots        = listField "x-fay-root-modules"  packageDesc
-      includePaths = listField "x-fay-include-paths" packageDesc
-      mSourceDir   = field     "x-fay-source-dir"    packageDesc
-      mOutputDir   = field     "x-fay-output-dir"    packageDesc
-      sourceDir    = fromMaybe (error "x-fay-source-dir missing") mSourceDir
-      outputDir    = fromMaybe (error "x-fay-output-dir missing") mOutputDir
+build :: PackageDescription -> Maybe FilePath -> IO ()
+build packageDesc pkgDb = do
+  let packages     = listField_ "x-fay-packages"      packageDesc
+      roots        = listField_ "x-fay-root-modules"  packageDesc
+      includePaths = listField_ "x-fay-include-paths" packageDesc
+      sourceDir    = field_     "x-fay-source-dir"    packageDesc
+      outputDir    = field_     "x-fay-output-dir"    packageDesc
 
   forM_ (zip roots [(1::Int)..]) $ \(name, i) -> do
     let candidate = sourceDir </> name <.> "hs"
@@ -34,18 +47,27 @@ foo packageDesc pkgDb = do
     if exists
       then do
         putStrLn $ "fay: [" ++ show i ++ " of " ++ show (length roots) ++ "] Compiling " ++ name ++ " ( " ++ candidate ++ ", " ++ out ++ " )"
-        compileFromTo (config pkgDb packages sourceDir includePaths) candidate (Just out)
+        compileFromTo (fayConfig pkgDb packages sourceDir includePaths) candidate (Just out)
       else
         error $ "fay-builder: Could not find " ++ candidate
 
-listField :: String -> PackageDescription -> [String]
-listField key = fromMaybe [] . fmap (map strip . splitOn ",") . field key
+listField :: String -> PackageDescription -> Maybe [String]
+listField key = fmap (map strip . splitOn ",") . field key
+
+listField_ :: String -> PackageDescription -> [String]
+listField_ = fromMaybe [] .: listField
+
+(.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+(.:) = (.).(.)
 
 field :: String -> PackageDescription -> Maybe String
 field key = fmap strip . lookup key . customFieldsPD
 
-config :: Maybe FilePath -> [String] -> FilePath -> [FilePath] -> CompileConfig
-config pkgDb packages dir includePs =
+field_ :: String -> PackageDescription -> String
+field_ key = fromMaybe (error $ key ++ "is  missing") . field key
+
+fayConfig :: Maybe FilePath -> [String] -> FilePath -> [FilePath] -> CompileConfig
+fayConfig pkgDb packages dir includePs =
     addConfigDirectoryIncludePaths (dir : includePs)
   . addConfigPackages              packages
   $ def { Fay.configWall        = True
@@ -55,3 +77,19 @@ config pkgDb packages dir includePs =
 
 strip :: String -> String
 strip = T.unpack . T.strip . T.pack
+
+defaultFayHook :: IO ()
+defaultFayHook = defaultMainWithHooks simpleUserHooks { postBuild = postBuildHook }
+
+postBuildHook :: Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+postBuildHook _ _ packageDesc localBuildInfo = do
+  putStrLn "Building Fay client ..."
+  build packageDesc  (findSpecificPackageDb localBuildInfo)
+  putStrLn "Finished building Fay client"
+  where
+    findSpecificPackageDb =
+        fmap (\(SpecificPackageDB p) -> p)
+      . find (\db -> case db of
+          SpecificPackageDB{} -> True
+          _                   -> False)
+      . withPackageDB
